856f4bb0478a1451d2ec7f7ac659ee18efcb2193
Khrystyna Klapushchak <klapushchak.khr@gmail.com>
2023-10-01 23:31:47 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+
+function scuberGreetingForFeet(ride){
+  if (ride<=400){
+    return 'This one is on me!'
+  } else if (ride<=2000){
+    return 'That will be twenty bucks.'
+  } else if (ride >= 2500){
+    return 'No can do.'
+  } else {
+    return 'I will gladly take your thirty bucks.'
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
+  return city==="NYC"? "Ok, sounds good." : "No go."
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tips){
+  switch(tips){
+    case 'generous':
+      return "Thank you so much.";
+      break;
+    case 'not as generous' :
+      return "Thank you.";
+      break;
+    default:
+      return 'Bye.'
+  }
   // Write your code here!
 }
